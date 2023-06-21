200b81169621b3909c90f82eacc3be8cdce39823
naledi5 <naledi.komape05@gmail.com>
2023-06-21 15:07:57 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+ 	Char Ch;
+
+	2023/06/16, 18;22 ;
+	Page; 4 ; of ; 8;
+
+       	Char Ch ;
+ 
+	for (Ch = 'z'; Ch >= 'a'; Ch--)
+ { 
+	 putchar(Ch);
+ }
+ 	putchar('\n');
+ return (0);
+}
